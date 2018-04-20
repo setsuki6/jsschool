94d0396e1445e241926cea73bd629f90f039626c
setsuki <s_tomo_6668@yahoo.co.jp>
2018-04-20 16:48:18 +0900

    180420変数系

---
+++ 180420/js/common.js
@@ -0,0 +1,71 @@
+console.log('test')
+var test = 'こんにちは'
+var num = 0
+
+console.log(test)
+console.log(num)
+
+
+var a = 1
+var b = 2
+
+a += b   //a = a + b
+a -= b
+a *= b
+a /= b
+
+
+
+console.log(a)
+
+console.log(10 % 3) //%はあまりを表す
+
+var c = 1
+c++
+c++
+c++ 
+c--
+
+
+//c++ →  c += 1
+//c-- →  c -= 1
+
+console.log(c)
+
+//文字列
+console.log('文字列',"文字列")
+console.log('0')
+
+//数値
+console.log(114514)
+
+//論理値
+console.log(true,false)
+
+console.log(typeof '0')
+console.log(typeof 0)
+console.log(typeof true)
+
+var d = 0
+var e = 1
+var f = 1
+var g = 2
+console.log(d === e)
+console.log(e === f)
+console.log(e !== f)
+console.log(d > e)
+console.log(d < e)
+console.log(e <= f)
+console.log(e >= f)
+console.log(!(e === f))
+console.log(!(d > f))
+
+var h = d === e
+console.log(!h)
+console.log(typeof h)
+
+var n = 100
+console.log(n > 0 && n < 20) //〇〇かつ〇〇
+console.log(n > 0 || n < 20) //〇〇OR〇〇
+
+
